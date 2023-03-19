b595130cb6138e41eb96c0472350684a317b295c
Weerapong Yenpayub <yenpayub_w@silpakorn.edu>
2023-03-19 12:04:29 +0700

    1

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -14,6 +14,8 @@ class _HomePageState extends State<HomePage> {
   List<Poll>? _polls;
   var _isLoading = false;
 
+  get child => null;
+
   @override
   void initState() {
     super.initState();
@@ -26,22 +28,256 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return MyScaffold(
-      body: Column(
-        children: [
-          Image.network('https://cpsu-test-api.herokuapp.com/images/election.jpg'),
-          Expanded(
-            child: Stack(
-              children: [
-                if (_polls != null) _buildList(),
-                if (_isLoading) _buildProgress(),
+    return Scaffold(
+      body:  Column(
+          children: [
+            Image.network('https://cpsu-test-api.herokuapp.com/images/election.jpg'),
+            Expanded(
+              child: Container(
+                //padding: const EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: Colors.blue.withOpacity(0.1),
+                ),
+                child: _page(),
+              ),
+            ),
+            /*Expanded(
+                child: Stack(
+                  children: [
+                    if (_polls != null) _buildList(),
+                    if (_isLoading) _buildProgress(),
+                  ],
+                )
+            ),*/
+
+          ],
+        ),
+
+      );
+  }
+
+  Widget _page() {
+      return SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                    '1.บุคคลใดที่คุณจะสนับสนุนให้เป็นนายกรัฐมนตรีในการเลือกตั้งครั้งนี้'
+                ),
               ],
             ),
-          ),
-        ],
-      ),
-    );
-  }
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: OutlinedButton(
+                onPressed: () {
+                },
+                child: const Text('พล.ต.อเสรีพิศุทธ์ เตมียเวส'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: OutlinedButton(
+                onPressed: () {
+                  /*debugPrint(month.toString());
+
+                setState(() {
+                  _selectedMonth = month;
+                });*/
+                },
+                child: const Text('นายกรณ์ จาติกวณิช'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: OutlinedButton(
+                onPressed: () {
+                  /*debugPrint(month.toString());
+
+              setState(() {
+                _selectedMonth = month;
+              });*/
+                }, child: const Text('นายอนุทิน ชาญวีรกูล'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: OutlinedButton(
+                onPressed: () {
+                  /*debugPrint(month.toString());
+
+              setState(() {
+                _selectedMonth = month;
+              });*/
+                }, child: const Text('พลเอกประยุทธ์ จันทร์โอชา'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: OutlinedButton(
+                onPressed: () {
+                  /*debugPrint(month.toString());
+
+              setState(() {
+                _selectedMonth = month;
+              });*/
+                }, child: const Text('คุณหญิงสุดารัตน์ เกยุราพันธ์ุ'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: OutlinedButton(
+                onPressed: () {
+                  /*debugPrint(month.toString());
+
+              setState(() {
+                _selectedMonth = month;
+              });*/
+                }, child: const Text('นส แพทองธาร ชินวัตร'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: OutlinedButton(
+                onPressed: () {
+                  /*debugPrint(month.toString());
+
+              setState(() {
+                _selectedMonth = month;
+              });*/
+                }, child: const Text('นายพิธา ลื้มเจริญรัตน์'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: OutlinedButton(
+                onPressed: () {
+                  /*debugPrint(month.toString());
+
+              setState(() {
+                _selectedMonth = month;
+              });*/
+                }, child: const Text('นพชลน่าน ศรีแก้ว'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: OutlinedButton(
+                onPressed: () {
+                  /*debugPrint(month.toString());
+
+              setState(() {
+                _selectedMonth = month;
+              });*/
+                }, child: const Text('อื่นๆ'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: OutlinedButton(
+                onPressed: () {
+                  /*debugPrint(month.toString());
+
+              setState(() {
+                _selectedMonth = month;
+              });*/
+                }, child: const Text('ยังหาคนที่เหมาะสมไม่ได้'),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                    '2.ในการเลือกตั้งสสแบบแบ่งเขต คุณจะเลือกผู้สมัครจากพรรคการเมืองใด'
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: OutlinedButton(
+                onPressed: () {
+                },
+                child: const Text('พรรคก้าวไกล'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: OutlinedButton(
+                onPressed: () {
+                },
+                child: const Text('พรรคไทยสร้างไทย'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: OutlinedButton(
+                onPressed: () {
+                },
+                child: const Text('พรรครวมไทยสร้างชาติ'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: OutlinedButton(
+                onPressed: () {
+                },
+                child: const Text('พรรคเพื่อไทย'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: OutlinedButton(
+                onPressed: () {
+                },
+                child: const Text('พรรคเสรีรวมไทย'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: OutlinedButton(
+                onPressed: () {
+                },
+                child: const Text('พรรคประชาธิปัตย์'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: OutlinedButton(
+                onPressed: () {
+                },
+                child: const Text('พรรคภูมิใจไทย'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: OutlinedButton(
+                onPressed: () {
+                },
+                child: const Text('พรรคพลังประชารัฐ'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: OutlinedButton(
+                onPressed: () {
+                },
+                child: const Text('อื่นๆ'),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                    '3.ในการเลือกตั้ง สส แบบบัญชี'
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
 
   ListView _buildList() {
     return ListView.builder(
